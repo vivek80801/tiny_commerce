a8d6e847bff9ee8e7ef840e1e533c4fb91f4ef70
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2026-09-10 07:35:32 +0530

    [Feat]: add categories management

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Schemas\Schema;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make("name")->required(),
+                Fieldset::make("category.image")
+                    ->label("Image")
+                    ->relationship("image")
+                    ->markAsRequired()
+                    ->schema([
+                        FileUpload::make("filename")
+                            ->disk("upload")
+                    ])
+                ,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CategoriesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("name"),
+                TextColumn::make("products_count")
+                    ->counts("products")
+                    ->label("Product Qty")
+                ,
+                ImageColumn::make("image.filename")
+                    ->label("Image")
+                    ->imageHeight(40)
+                    ->circular()
+                    ->disk("upload")
+                ,
+
+                TextColumn::make("created_at")->since(),
+                TextColumn::make("updated_at")->since(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
